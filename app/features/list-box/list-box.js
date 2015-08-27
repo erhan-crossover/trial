e0b5cdb21b354cad6b3db5180e1586464165f43e
erhan-crossover <erhan.crossover@gmail.com>
2015-08-27 13:07:22 +0200

    list item, list box and other features implemented

--- app/features/list-box/list-box.js
+++ app/features/list-box/list-box.js
@@ -3,16 +3,92 @@
   'use strict';
 
   app
-    .directive('listBox', [function () {
+    .directive('listBox', ['$timeout', function ($timeout) {
       return {
         restrict: 'A',
         require: 'listItem',
         controller: function($scope) {
+          var self = this;
           this.data = $scope.data;
+
+          $timeout(function() {
+            renderChart('unit');
+            renderChart('func');
+          });
+
+
+          this.getFailedTask = function() {
+            if (self.data.metrics.state === 'rejected') {
+              return 'Metrics';
+            } else if (self.data.build.state === 'rejected') {
+              return 'Build';
+            } else if (self.data.unitTest.state === 'rejected') {
+              return 'Unit Tests';
+            } else if (self.data.functionalTest.state === 'rejected') {
+              return 'Functional Tests';
+            }
+          };
+
+          function renderChart(chartType) {
+
+            // we do not have chart data so return
+            if (chartType === 'unit') {
+              if (!self.data.unitTest) {
+                return;
+              }
+              if (self.data.unitTest &&
+                  self.data.unitTest.state === 'pending' ||
+                  self.data.unitTest.state === 'running') {
+                return;
+              }
+            }
+
+            if (chartType === 'func') {
+              if (!self.data.functionalTest) {
+                return;
+              }
+              if (self.data.functionalTest.state === 'pending' ||
+                  self.data.functionalTest.state === 'running') {
+                return;
+              }
+            }
+
+            var elementClass;
+            var data = {
+              series: []
+            };
+
+            var sum = function(a, b) { return a + b };
+
+            if (chartType === 'unit') {
+              data.series = [self.data.unitTest.count, self.data.unitTest.passed];
+              elementClass = '.unit-test-chart';
+            } else {
+              data.series = [self.data.functionalTest.count, self.data.functionalTest.passed];
+              elementClass = '.func-test-chart';
+            }
+
+            if (elementClass) {
+              new Chartist.Pie(elementClass, data, {
+                labelInterpolationFnc: function(value) {
+                  return Math.round(value / data.series.reduce(sum) * 100) + '%';
+                }
+              });
+            }
+          }
+
+          $scope.$watch('selected', function() {
+            $timeout(function() {
+              renderChart('unit');
+              renderChart('func');
+            });
+          });
+
         },
         controllerAs: 'taskBox',
         scope: {
-          data: '='
+          data: '=',
+          selected: '='
         },
         templateUrl: 'features/list-box/list-box.html'
       }
--- app/features/list-item/list-item.js
+++ app/features/list-item/list-item.js
@@ -3,16 +3,43 @@
   'use strict';
 
   app
-    .directive('listItem', [function () {
+    .directive('listItem', ['$timeout', function ($timeout) {
       return {
         restrict: 'A',
         require: 'list',
+        transclue: true,
         controller: function($scope) {
+          var self = this;
           this.data = $scope.data;
+          this.itemIndex = $scope.itemIndex;
+          this.selectedIndex = 0;
+
+          this.expandDetails = function(selectedIndex) {
+            var self = this;
+
+            $('.detail').each(function() {
+              $(this)
+                .removeClass('detail')
+                .find('.list-item-details')
+                .hide();
+            });
+
+            $('#list-item-index-' + selectedIndex)
+              .addClass('detail')
+              .find('.list-item-details')
+              .show();
+
+            $timeout(function() {
+              self.showDetails = !self.showDetails;
+              self.selectedIndex = selectedIndex;
+            });
+
+          };
         },
         controllerAs: 'taskItem',
         scope: {
-          data: '='
+          data: '=',
+          selectedIndex: '='
         },
         templateUrl: 'features/list-item/list-item.html'
       }
